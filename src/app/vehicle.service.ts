0d5d78319043692336b041aecff58c3026f88fe5
Devil983 <62641503+Devil983@users.noreply.github.com>
2021-01-21 11:18:17 +0530

    update

---
+++ src/app/vehicle.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehicleService {
+  httpClient:HttpClient
+  
+
+  url=`http://localhost:8080/user/vehicle`
+
+  constructor(httpClient:HttpClient) { 
+    this.httpClient=httpClient
+  }
+
+
+  getVehicles(){
+    const userid=sessionStorage.getItem('id')
+//test
+
+    return this.httpClient.get('http://localhost:8080/user/vehiclesByUserid/'+userid)
+    
+  }
+
+
+  getVehicleDetails(id) {
+    // add the token in the request header
+    const httpOptions = {
+     headers: new HttpHeaders({
+      //  token: sessionStorage['token']
+
+     })
+   };
+  console.log(this.url+"/"+id)
+
+   return this.httpClient.get(this.url  + "/"+id, httpOptions)
+  }
+
+
+  deleteVehicle(id) {
+    // add the token in the request header
+    const httpOptions = {
+     headers: new HttpHeaders({
+      //  token: sessionStorage['token']
+
+     })
+   };
+  console.log(this.url+"/"+id)
+
+   return this.httpClient.delete('http://localhost:8080/user/vehicle' + "/"+id, httpOptions)
+  }
+
+  updateVehicle(v_id:number,v_company_name: string, v_model: string, v_regNo: string) {
+    // add the token in the request header
+
+    console.log(v_id+"vehicle id")
+    const httpOptions = {
+     headers: new HttpHeaders({
+      //  token: sessionStorage['token']
+     })
+   };
+
+   const body = {
+    v_id:v_id,
+    v_company_name: v_company_name,
+    v_model: v_model,
+    v_regNo: v_regNo,
+    
+   }
+   
+   return this.httpClient.put(this.url , body, httpOptions)
+ }
+
+ insertVehicle(v_company_name: string, v_model: string, v_regNo: string) {
+   // add the token in the request header
+   const httpOptions = {
+    headers: new HttpHeaders({
+      // token: sessionStorage['token']
+    })
+  };
+
+  const u_id=sessionStorage.getItem('id')
+
+  const body = {
+    v_company_name: v_company_name,
+    v_model: v_model,
+    v_regNo: v_regNo,
+    v_user:{
+      u_id:u_id
+    }
+  }
+
+  console.log("userid "+u_id)
+  
+  return this.httpClient.post(this.url + "/create", body, httpOptions)
+}
+
+}
