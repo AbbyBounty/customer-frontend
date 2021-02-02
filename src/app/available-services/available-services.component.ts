c73665a70a4d6b7f45fb2907cce6c7077cc2bdbc
abhilash <abhilash.kamble376@gmail.com>
2021-02-02 10:57:00 +0530

    update

--- src/app/available-services/available-services.component.ts
+++ src/app/available-services/available-services.component.ts
@@ -1,3 +1,4 @@
+import { OrderService } from './../order.service';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { VehicleService } from 'app/vehicle.service';
@@ -14,14 +15,20 @@ export class AvailableServicesComponent implements OnInit {
 
   selectedServices: FormGroup;
 
-  serviceTaken=[]
+  serviceTaken = []
   allServices = []
   services = []
-  selectedVehicle=""
-  shop=""
+  selectedVehicle = ""
+  shop = ""
 
-  shopDetails=[]
-  constructor(private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private shopDetailsService: ShopDetailsService, private vehicleService: VehicleService, private toastr: ToastrService, private router: Router) {
+  shopDetails = []
+  constructor(private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private shopDetailsService: ShopDetailsService,
+    private vehicleService: VehicleService,
+    private toastr: ToastrService,
+    private router: Router,
+    private orderService: OrderService) {
     this.selectedServices = this.formBuilder.group({
       website: this.formBuilder.array([], [Validators.required])
     })
@@ -29,9 +36,9 @@ export class AvailableServicesComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadServices()
-    this.selectedVehicle=sessionStorage.getItem("selectedVehicle")
-    this.shop=sessionStorage.getItem("shopDetails")
-    console.log(`shop name : `+this.shop)
+    this.selectedVehicle = sessionStorage.getItem("selectedVehicle")
+    this.shop = sessionStorage.getItem("shopDetails")
+    console.log(`shop name : ` + this.shop)
     this.loadCart()
   }
 
@@ -51,58 +58,78 @@ export class AvailableServicesComponent implements OnInit {
 
   onCheckboxChange(e) {
     const website: FormArray = this.selectedServices.get('website') as FormArray;
-  
+
     if (e.target.checked) {
       website.push(new FormControl(e.target.value));
     } else {
-       const index = website.controls.findIndex(x => x.value === e.target.value);
-       website.removeAt(index);
+      const index = website.controls.findIndex(x => x.value === e.target.value);
+      website.removeAt(index);
     }
   }
-    
-  submit(){
+
+  submit() {
     console.log(this.selectedServices.value);
-    this.serviceTaken=this.selectedServices.value
-    sessionStorage.setItem('selectedServices',this.selectedServices.value.website)
+    this.serviceTaken = this.selectedServices.value
+    sessionStorage.setItem('selectedServices', this.selectedServices.value.website)
 
   }
 
-  addToCart(service,vehicle){
+  addToCart(service, vehicle) {
     const id = this.activatedRoute.snapshot.queryParams['id']
 
-  if(vehicle){
-    console.log(service)
-      this.toastr.success('Added to Cart  ',this.selectedServices.value.website,{
-        positionClass:'toast-top-right',
-        closeButton:true,
-        progressAnimation:'decreasing',
-        titleClass:'toast-title'
+    if (vehicle) {
+      console.log(service)
+      this.toastr.success('Added to Cart  ', this.selectedServices.value.website, {
+        positionClass: 'toast-top-right',
+        closeButton: true,
+        progressAnimation: 'decreasing',
+        titleClass: 'toast-title'
       })
 
-      sessionStorage.setItem("selectedVehicle",this.selectedVehicle)
+      sessionStorage.setItem("selectedVehicle", this.selectedVehicle)
+      sessionStorage.setItem("v_id", this.selectedVehicle)
+      sessionStorage.setItem("ven_id", this.selectedVehicle)
 
-      
+
+
+    }
+
+    this.shopDetailsService
+      .getVendorDetails(id)
+      .subscribe(response => {
+        if (response['status'] == 'success') {
+          console.log(response['data']['ven_shop_name'])
+          sessionStorage.setItem("shopDetails", response['data']['ven_shop_name'])
+
+        }
+      })
+
 
   }
 
-  this.shopDetailsService
-  .getVendorDetails(id)
-  .subscribe(response => {
-    if (response['status'] == 'success') {
-      console.log(response['data']['ven_shop_name'])
-      sessionStorage.setItem("shopDetails",response['data']['ven_shop_name'])
-      
-    }
-  })
+  loadCart() {
+
+  }
+
+  addOrder() {
+    this.orderService
+      .addOrder()
+      .subscribe(response => {
+        if (response['status'] == 'success') {
+          console.log(response)
+          this.toastr.success('Order placed ', "", {
+            positionClass: 'toast-top-right',
+            closeButton: true,
+            progressAnimation: 'decreasing',
+            titleClass: 'toast-title'
+          })
+        }
+
+      })
+
+    this.router.navigate(['/dashboard'])
+
+  }
 
 
-}
-
-loadCart(){
-  
-}
-
-placeOrder(){
-  
-}
 }
